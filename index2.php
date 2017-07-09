9bab4d8c348a2c5eb33c4fc961ffe82bf40a76a8
Alfonso Fernandez <le_debutant@yahoo.fr>
2017-07-09 14:26:27 +0200

    commentaire dans index2.php

--- index2.php
+++ index2.php
@@ -1,7 +1,19 @@
 <?php 
 include_once("model/fonction.php");
 
-
+/**
+* Alfonso: l'index.php devrait être ce fichier. J'imagine que tu as rapatrier
+ * un template qui avait un index.html et tu l'as renommé pour pouvoir tester
+ * les styles.
+ *
+ * Attention à faire trop ceci car ça breaker le système.
+ * l'index.html que tu as importé est sensé être une vue qui doit résider dans le
+ * dossier view/ et tu dois donc ajusté les chemin js/ et css/
+ * à priori non puisqu'on appel les vu depuis l'index2.php
+ * (qui doit s'appeler index.php)
+ * Donc à mon avis tu aurais pu l'insérer direment dans view/
+ * On en reparle.
+ */
 $page=getpage();
 
 switch($page){
